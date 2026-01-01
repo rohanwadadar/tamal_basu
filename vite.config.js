2f48a9ae6ad3905c0580a186a07819c31764ddb8
Rohan Wadadar <135444253+rohanwadadar@users.noreply.github.com>
2026-01-01 20:42:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/tamal_basu/',
+  base: '/tamal_basu/',  // REQUIRED for GitHub / Netlify subpath
   plugins: [react()],
 })
