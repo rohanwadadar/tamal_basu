7686851a209853c35469da70bb346b013ef6f577
Rohan Wadadar <135444253+rohanwadadar@users.noreply.github.com>
2026-01-10 09:51:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
-import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
-import Experience from "./components/Experience";
-import Research from "./components/Research";
-//import Publications from "./components/Publications";
-import HeroAbout from "./components/Hero";
-import Contact from "./components/Contact";
-import EducationRoadmap from "./components/education";
+// import Navbar from "./components/Navbar";
+// // import Hero from "./components/Hero";
+// // import About from "./components/About";
+// import Experience from "./components/Experience";
+// import Research from "./components/Research";
+// //import Publications from "./components/Publications";
+// import HeroAbout from "./components/Hero";
+// import Contact from "./components/Contact";
+// import EducationRoadmap from "./components/education";
 
-export default function App() {
-  return (
-    <div className="bg-white text-gray-800">
+// export default function App() {
+//   return (
+//     <div className="bg-white text-gray-800">
 
-      {/* NAVBAR */}
-      <Navbar />
+//       {/* NAVBAR */}
+//       <Navbar />
 
-      {/* HERO */}
-      <section className="bg-gradient-to-br from-primary to-primaryDark text-white">
-        <HeroAbout />
-      </section>
+//       {/* HERO */}
+//       <section className="bg-gradient-to-br from-primary to-primaryDark text-white">
+//         <HeroAbout />
+//       </section>
 
-      {/* EDUCATION */}
-      <section className="bg-primaryLight">
-        <EducationRoadmap />
-      </section>
+//       {/* EDUCATION */}
+//       <section className="bg-primaryLight">
+//         <EducationRoadmap />
+//       </section>
 
-      {/* EXPERIENCE */}
-      <section className="bg-white">
-        <Experience />
-      </section>
+//       {/* EXPERIENCE */}
+//       <section className="bg-white">
+//         <Experience />
+//       </section>
 
-      {/* RESEARCH */}
-      <section className="bg-primaryLight">
-        <Research />
-      </section>
+//       {/* RESEARCH */}
+//       <section className="bg-primaryLight">
+//         <Research />
+//       </section>
 
-      {/* PUBLICATIONS
-      <section className="bg-white">
-        <Publications />
-      </section> */}
+//       {/* PUBLICATIONS
+//       <section className="bg-white">
+//         <Publications />
+//       </section> */}
 
-      {/* CONTACT */}
-      <section className="bg-gradient-to-r from-primary to-primaryDark text-white">
-        <Contact />
-      </section>
-    </div>
-  );
-}
+//       {/* CONTACT */}
+//       <section className="bg-gradient-to-r from-primary to-primaryDark text-white">
+//         <Contact />
+//       </section>
+//     </div>
+//   );
+// }
